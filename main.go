83a8f241ed5a8a3ce9493bacf775fe60972a8c73
Adam Talbot <adam.talbot@fansz.com>
2015-10-29 12:22:00 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"github.com/fvbock/endless"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	endless.ListenAndServe(":8000", nil)
+}
